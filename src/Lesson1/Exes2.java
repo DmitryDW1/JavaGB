f4f15bb1e6256ba0987f1c46e1228ffb9cb6a4d7
DmitryDW1 <DmitryDW1@yandex.ru>
2023-06-04 18:18:32 +0600

    create lesson1

---
+++ src/Lesson1/Exes2.java
@@ -0,0 +1,33 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Exes2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("zadanie2(n, scanner) = " + zadanie2(n, scanner));
+
+    }
+
+    /**
+     * @param n      int
+     * @param scanner scanner
+     * @return count
+     * @apiNote Дана последовательность N целых чисел. Найти количество положительных чисел, после которых следует
+     * отрицательное число.
+     */
+    private static int zadanie2(int n, Scanner scanner) {
+
+        int count = 0;
+        int a = 0, b = 0;
+        a = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            b = scanner.nextInt();
+            if (a > 0 && b < 0) count++;
+            a = b;
+        }
+        scanner.close();
+        return count;
+    }
+}
