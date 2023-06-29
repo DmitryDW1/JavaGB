25ca43acabdc6ecaf1852eddb169194f32649a10
DmitryDW1 <DmitryDW1@yandex.ru>
2023-06-29 20:13:11 +0600

    Create Seminar6 HomeWork

---
+++ src/main/java/Seminar6/HomeWork/listNotebooks.java
@@ -0,0 +1,60 @@
+package Seminar6.HomeWork;
+
+public class listNotebooks {
+    static String color;
+    static String os;
+    static String model;
+    static Double displaySize;
+    static Integer sddSize;
+    static Integer ramSize;
+    static Double price;
+    static String manufacturer;
+    public static String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public static String getOs(){
+        return os;
+    }
+    public void setOs(String os){
+        this.os = os;
+    }
+    public static String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public static Double getDisplaySize(){
+        return displaySize;
+    }
+    public void setDisplaySize(Double displaySize){
+        this.displaySize = displaySize;
+    }
+    public static Integer getSddSize(){
+        return sddSize;
+    }
+    public void setSddSize(Integer sddSize){
+        this.sddSize = sddSize;
+    }
+    public static Integer getRamSize(){
+        return ramSize;
+    }
+    public void setRamSize(Integer ramSize){
+        this.ramSize = ramSize;
+    }
+    public static Double getPrice(){
+        return price;
+    }
+    public void setPrice(Double price){
+        this.price = price;
+    }
+    public static String getManufacturer(){
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer){
+        this.manufacturer = manufacturer;
+}
+}
