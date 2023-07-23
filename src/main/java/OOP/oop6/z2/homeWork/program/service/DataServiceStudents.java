de45da45dcddaa2acadb33b340c88acd0d9ab707
DmitryDW1 <DmitryDW1@yandex.ru>
2023-07-24 00:20:58 +0600

    Change oop6 homework

---
+++ src/main/java/OOP/oop6/z2/homeWork/program/service/DataServiceStudents.java
@@ -0,0 +1,9 @@
+package OOP.oop6.z2.homeWork.program.service;
+
+import OOP.oop6.z2.homeWork.program.data.Student;
+
+import java.util.List;
+
+public interface DataServiceStudents {
+    void show (List<Student> students);
+}
