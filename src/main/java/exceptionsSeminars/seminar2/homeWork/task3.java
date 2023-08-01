0d948cfcc1ebaa5a67d5700e3a5ac5184b9b92cf
DmitryDW1 <DmitryDW1@yandex.ru>
2023-08-01 20:45:03 +0600

    Final change exceptionsSeminars seminar2 homeWork

--- src/main/java/exceptionsSeminars/seminar2/homeWork/task3.java
+++ src/main/java/exceptionsSeminars/seminar2/homeWork/task3.java
@@ -2,6 +2,23 @@ package exceptionsSeminars.seminar2.homeWork;
 
 public class task3 {
     public static void main(String[] args) throws Exception {
+        // исходный код
+//        try {
+//            int a = 90;
+//            int b = 3;
+//            System.out.println(a / b);
+//            printSum(23, 234);
+//            int[] abc = { 1, 2};
+//            abc[3] = 9;
+//        } catch (Throwable ex) {
+//            System.out.println("Что-то пошло не так...");
+//        } catch (NullPointerException ex) {
+//            System.out.println("Указатель не может указывать на null!");
+//        } catch (IndexOutOfBoundsException ex) {
+//            System.out.println("Массив выходит за пределы своего размера!");
+//        }
+
+        // Пример изменений
         try {
             int a = 90;
             int b = 3;
